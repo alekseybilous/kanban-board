acdaa74acee8edc07f9cbfddef5002645ee817b1
Oleksii Bilous <oleksii.bilous@ext.hellofresh.com>
2025-06-20 14:17:28 +0300

    feat(storybook): add storybook host

---
+++ packages/storybook-host/README.md
@@ -0,0 +1,7 @@
+# @kanban-board/storybook-host
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @kanban-board/storybook-host` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ packages/storybook-host/src/lib/storybook-host.tsx
@@ -0,0 +1,11 @@
+import styles from './storybook-host.module.css';
+
+export function KanbanBoardStorybookHost() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to KanbanBoardStorybookHost!</h1>
+    </div>
+  );
+}
+
+export default KanbanBoardStorybookHost;
